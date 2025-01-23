6e4df46c73e61a880c2f951d5baf3c7ca08afe2c
Kuna Lakshmi Vara Prasad <kunalakshmi.prasad@nw18.com>
2025-01-23 21:41:21 +0530

    fixed rating ui and race condition

--- src/pages/BookDetailPage.jsx
+++ src/pages/BookDetailPage.jsx
@@ -32,8 +32,8 @@ const BookDetailPage = () => {
         body: JSON.stringify(comment),
       })
         .then(() => {
-          setComments(
-            (prev) => getSingleBookLoader({ params: { bookId } }).reviews
+          getSingleBookLoader({ params: { bookId } }).then((data) =>
+            setComments(() => data.reviews)
           );
         })
         .catch((error) => setComments((prev) => prev));
@@ -76,7 +76,7 @@ const BookDetailPage = () => {
                   {book.rating.toFixed(1)}
                 </span>
               </div>
-              <p className="text-gray-700 dark:text-gray-300">
+              <p className="text-gray-700 dark:text-gray-300 text-left">
                 {book.description}
               </p>
             </div>
@@ -110,11 +110,12 @@ const BookDetailPage = () => {
                   type="button"
                   className={`w-6 h-6 ${
                     rating <= newRating ? "text-yellow-400" : "text-gray-300"
-                  } focus:outline-none`}
+                  } focus:outline-none relative`}
                   onClick={() => setNewRating(rating)}
                 >
                   <svg
-                    className="w-full h-full text-yellow-400 "
+                    key={rating}
+                    className={`w-5 h-5 text-yellow-400 absolute bottom-0 left-2.5 `}
                     fill="currentColor"
                     viewBox="0 0 20 20"
                   >
@@ -138,43 +139,47 @@ const BookDetailPage = () => {
           <h2 className="text-2xl font-bold mb-4 text-gray-900 dark:text-white">
             Comments
           </h2>
-          {comments.map((comment) => (
-            <div
-              key={comment.id}
-              className="mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg"
-            >
-              <div className="flex items-start space-x-4">
-                <div className="flex-shrink-0">
-                  <img
-                    src={`https://api.dicebear.com/6.x/avataaars/svg?seed=${comment._id} `}
-                    alt="User avatar"
-                    className="w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-600"
-                  />
-                </div>
-                <div className="flex-1">
-                  <div className="flex items-center">
-                    {[...Array(5)].map((_, i) => (
-                      <svg
-                        key={i}
-                        className={`w-4 h-4 ${
-                          i < comment.rating
-                            ? "text-yellow-400"
-                            : "text-gray-300"
-                        }`}
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                    ))}
+          {comments.length ? (
+            comments.map((comment) => (
+              <div
+                key={comment.id}
+                className="mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg"
+              >
+                <div className="flex items-start space-x-4">
+                  <div className="flex-shrink-0">
+                    <img
+                      src={`https://api.dicebear.com/6.x/avataaars/svg?seed=${comment._id} `}
+                      alt="User avatar"
+                      className="w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-600"
+                    />
                   </div>
-                  <p className="mb-2 text-gray-700 dark:text-gray-300 text-left">
-                    {comment.reviewText}
-                  </p>
+                  <div className="flex-1">
+                    <div className="flex items-center">
+                      {[...Array(5)].map((_, i) => (
+                        <svg
+                          key={i}
+                          className={`w-4 h-4 ${
+                            i < comment.rating
+                              ? "text-yellow-400"
+                              : "text-gray-300"
+                          }`}
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                        </svg>
+                      ))}
+                    </div>
+                    <p className="mb-2 text-gray-700 dark:text-gray-300 text-left">
+                      {comment.reviewText}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <div> Feel free to comment first 🚀</div>
+          )}
         </div>
       </div>
     </div>
